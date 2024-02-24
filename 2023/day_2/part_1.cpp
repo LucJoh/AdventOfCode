993234cb980a007eb28601fdc6d0d548981aba8d
lucjoh <lucjoh99@hotmail.com>
2024-02-24 17:42:43 +0100

    .

--- 2023/day_2/part_1.cpp
+++ 2023/day_2/part_1.cpp
@@ -6,38 +6,71 @@
 #include <cstring>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <typeinfo>
 using namespace std;
+
+// 12 red cubes, 13 green cubes, and 14 blue cubes
 
 int main() {
 
 	std::vector<std::string> numbers;
-	int tot, sum=0, i=0, j=0, k=0;
+	int n;
+	int sum = 0;
+	int red = 12;
+	int green = 13;
+	int blue = 14;
+	int game_id;
+	bool impossible_game = false;
 	string line;
 	ifstream file("input.txt");
 	if (file.is_open()) {
 		while (getline(file, line)) {
 			string num;
 			string str = line;
-			string str_temp;
-			tot = str.length();
-			for(i=0; i<tot; i++) {
-				str_temp.push_back(str[i]);
-				if(str[i]>='0' && str[i]<='9') {
-					num += str[i];
-				} else if (str_temp.find(digits_text[0]) != std::string::npos) {
-					num += digits[0];
-					str_temp = str[i];
-				} else if (str_temp.find(digits_text[1]) != std::string::npos) {
-					num += digits[1];
-					str_temp = str[i];
-				} else if (str_temp.find(digits_text[2]) != std::string::npos) {
-					num += digits[2];
-					str_temp = str[i];
+			string word = "";
+			string prev_word = "";
+			n = str.length();
+			for (int i = 0; i < n; i++) {
+				if (str[i] == ' ' or str[i] == ';' or str[i] == ',' or str[i] == ':' or i == (n-1)) {
+					if (prev_word == "Game") {
+						game_id = stoi(word);
+					}
+					else if (word == "red," or word == "red;" or word == "red" or word == "re") {
+						if (stoi(prev_word) > red) {
+							impossible_game = true;
+							//cout << "not possible red was" << prev_word << endl;
+						}
+					}
+					else if (word == "green," or word == "green;" or word == "green" or word == "gree") {
+						if (stoi(prev_word) > green) {
+							impossible_game = true;
+							//cout << "not possible green was" << prev_word << endl;
+						}
+					}
+					else if (word == "blue," or word == "blue;" or word == "blue" or word == "blu") {
+						if (stoi(prev_word) > blue) {
+							impossible_game = true;
+							//cout << "not possible blue was" << prev_word << endl;
+						}
+					}
+					prev_word = word;
+					word = "";
+				} 
+				else { 
+					word += str[i];
 				}
-				//cout << line;
-				//cout << "\n";
 			}
-			file.close();
+			if (impossible_game == false) {
+				sum += game_id;
+				//cout << "possible, adding " << game_id << endl;
+				//cout << "sum is " << sum << endl;
+			}
+			impossible_game = false;
 		}
-		return 0;
+		file.close();
+		cout << sum << endl;
 	}
+	return 0;
+}
---
+++ 2023/day_2/part_2.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+// 12 red cubes, 13 green cubes, and 14 blue cubes
+
+int main() {
+
+	std::vector<std::string> numbers;
+	int n;
+	int sum = 0;
+	int sum_part_2 = 0;
+	int red = 12;
+	int green = 13;
+	int blue = 14;
+	int biggest_red = 0;
+	int biggest_green = 0;
+	int biggest_blue = 0;
+	int game_id;
+	bool impossible_game = false;
+	string line;
+	ifstream file("input.txt");
+	if (file.is_open()) {
+		while (getline(file, line)) {
+			string num;
+			string str = line;
+			string word = "";
+			string prev_word = "";
+			n = str.length();
+			for (int i = 0; i < n; i++) {
+				if (str[i] == ' ' or str[i] == ';' or str[i] == ',' or str[i] == ':' or i == (n-1)) {
+					if (prev_word == "Game") {
+						game_id = stoi(word);
+					}
+					else if (word == "red," or word == "red;" or word == "red" or word == "re") {
+						if (stoi(prev_word) > biggest_red) biggest_red = stoi(prev_word);
+						if (stoi(prev_word) > red) {
+							impossible_game = true;
+							//cout << "not possible red was" << prev_word << endl;
+						}
+					}
+					else if (word == "green," or word == "green;" or word == "green" or word == "gree") {
+						if (stoi(prev_word) > biggest_green) biggest_green = stoi(prev_word);
+						if (stoi(prev_word) > green) {
+							impossible_game = true;
+							//cout << "not possible green was" << prev_word << endl;
+						}
+					}
+					else if (word == "blue," or word == "blue;" or word == "blue" or word == "blu") {
+						if (stoi(prev_word) > biggest_blue) biggest_blue = stoi(prev_word);
+						if (stoi(prev_word) > blue) {
+							impossible_game = true;
+							//cout << "not possible blue was" << prev_word << endl;
+						}
+					}
+					prev_word = word;
+					word = "";
+				} 
+				else { 
+					word += str[i];
+				}
+			}
+			if (impossible_game == false) {
+				sum += game_id;
+				//cout << "possible, adding " << game_id << endl;
+				//cout << "sum is " << sum << endl;
+			}
+			impossible_game = false;
+			sum_part_2 += (biggest_red * biggest_green * biggest_blue);
+			biggest_red = 0;
+			biggest_green = 0;
+			biggest_blue = 0;
+		}
+		file.close();
+		//cout << sum << endl;
+		cout << sum_part_2 << endl;
+	}
+	return 0;
+}

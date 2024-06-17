a06bca6a1db2b5b4a41ac162dabb335e745655df
lucjoh <lucjoh99@hotmail.com>
2024-06-17 23:25:01 +0200

    started day 3

---
+++ 2023/day_3/part_1.cpp
@@ -0,0 +1,35 @@
+#include <fstream> 
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+int main() {
+
+	string line;
+	//ifstream input("calibration.txt");
+
+	std::fstream input("calibration.txt", std::ios_base::in);
+
+	// Check if the file is successfully opened 
+	if (!input.is_open()) { 
+		cerr << "Error opening the file!" << endl; 
+		return 1; 
+	}
+
+    int a;
+    while (input >> a)
+    {
+        printf("%d ", a);
+    }
+
+    getchar();
+
+//	while (getline(input, line)) { 
+//		cout << line << endl; 
+//	} 
+
+	input.close(); 
+
+	return 0;
+}

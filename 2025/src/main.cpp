c14d1bf35522c8f1780672b81a303f79ab0e9db0
lucjoh <lucjoh99hotmail.com>
2026-01-03 16:26:20 +0100

    simplify main.cpp

--- 2025/src/main.cpp
+++ 2025/src/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
+#include <functional>
 #include <iostream>
+#include <map>
 
 void day1_1();
 void day1_2();
@@ -17,30 +19,17 @@ int main(int argc, char **argv) {
   int day = std::stoi(argv[1]);
   int part = std::stoi(argv[2]);
 
-  switch (day) {
-  case 1:
-    if (part == 1) {
-      day1_1();
-    } else {
-      day1_2();
-    }
-    break;
-  case 2:
-    if (part == 1) {
-      day2_1();
-    } else {
-      day2_2();
-    }
-    break;
-  case 3:
-    if (part == 1) {
-      day3_1();
-    } else {
-      //      day3_2();
-    }
-    break;
-  default:
-    std::cerr << "Day " << day << " not implemented\n";
+  std::map<std::pair<int, int>, std::function<void()>> functions = {
+      {{1, 1}, day1_1}, {{1, 2}, day1_2}, {{2, 1}, day2_1},
+      {{2, 2}, day2_2}, {{3, 1}, day3_1}};
+
+  auto it = functions.find({day, part});
+  if (it != functions.end()) {
+    it->second();
+  } else {
+    std::cerr << "Day " << day << " Part " << part << " not implemented\n";
     return 1;
   }
+
+  return 0;
 }
